f640863d9e60caeef03855db1eedbb235e8cc1ee
Sushen Satturu <sushensatturu25@gmail.com>
2024-09-07 10:33:57 +1000

    Base db setup

--- myhackernews/app.py
+++ myhackernews/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 from myHackerNews import get_top_news
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:8080'])
 
+db_uri = "postgresql://admin:admin@localhost:5432/mydb"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri  # Change to your database URI
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route("/")
+from models import Post
+
+
+@app.route("/", methods=['GET'])
 def hello_from_root():
     return jsonify(message='Hello from root!')
+
+@app.route("/")
 
 
 @app.route("/get_top_news")
---
+++ myhackernews/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    summary = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<Post {self.id} - {self.title}>"
